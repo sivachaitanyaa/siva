5e5c624c3da2d7454870837044c822520d4dddc0
sivachaitanyaa <sivachaitanya777@gmail.com>
2023-02-13 15:11:14 +0530

    intial commit

---
+++ src/test/java/com/sloakm/springwithhybernate/LambokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sloakm.springwithhybernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LambokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
